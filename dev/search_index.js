c0b6f24efba77549f97333b1c38d35638ae9204b
Documenter.jl <documenter@juliadocs.github.io>
2020-11-14 19:05:12 +0000

    build based on 8e7fbef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PowerMarketAdeq","category":"page"},{"location":"#PowerMarketAdeq","page":"Home","title":"PowerMarketAdeq","text":"","category":"section"},{"location":"#This-is-not-maintained","page":"Home","title":"This is not maintained","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PowerMarketAdeq]","category":"page"}]
+}
